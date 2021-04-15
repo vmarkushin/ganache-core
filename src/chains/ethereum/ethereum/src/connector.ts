9af9e68f7e0ec444739e4d02db59695836ffe644
Mike Seese <seesemichaelj@gmail.com>
2021-04-15 16:19:21 -0700

    remove unnecessary constructor check for old fallback implementation

--- src/chains/ethereum/ethereum/src/connector.ts
+++ src/chains/ethereum/ethereum/src/connector.ts
@@ -22,7 +22,6 @@ function isHttp(
 ): connection is HttpRequest {
   return (
     connection.constructor.name === "uWS.HttpRequest" ||
-    connection.constructor.name === "RequestWrapper" ||
     connection.constructor.name === "HttpRequest"
   );
 }
